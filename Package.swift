c48c371a83668fc17b6152dcea8822430fa6b3e0
felfoldy <tiboofficial@gmail.com>
2026-04-03 14:36:48 +0200

    Update SwiftPy required version

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/felfoldy/SwiftPy", from: "0.17.0"),
+        .package(url: "https://github.com/felfoldy/SwiftPy", from: "0.19.0"),
         .package(url: "https://github.com/ibrahimcetin/libgit2.git", exact: "1.9.2"),
         // .package(url: "https://github.com/ibrahimcetin/SwiftGitX", branch: "main"),
     ],
